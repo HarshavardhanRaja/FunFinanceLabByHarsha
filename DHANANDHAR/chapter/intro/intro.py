08e27f337bbbf51562882866856017d3ce0ca784
Harshavardhan <harshaiitg97@gmail.com>
2026-05-14 16:57:24 +0530

    test

---
+++ DHANANDHAR/chapter/intro/intro.py
@@ -0,0 +1,52 @@
+"""
+
+Are you from Middle class?
+
+Then this series is for you.
+
+We were told...
+
+Study hard...
+get a good job...
+and life will be set.
+
+But what actually happens?
+
+Salary comes in...
+
+E M I goes out...
+
+and life...
+
+stays exactly the same.
+
+I was there too.
+
+Then I realized...
+
+the problem is not money.
+
+The problem is the system.
+And to understand that system...
+
+I created this series called Dhanandhar.
+A 10 chapter journey to master personal finance.
+
+This series will help you understand...
+
+making money is not enough.
+
+You also need to learn...
+
+how to manage it...
+and how to grow it.
+
+Because financial freedom...
+
+is not only for rich people.
+
+Follow this series...
+
+and let's build a smarter financial life together.
+
+"""
